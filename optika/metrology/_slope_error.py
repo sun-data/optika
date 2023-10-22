9a82e2955c15397d7e4d01f34f16b6b4c364cd2d
Roy Smart <roytsmart@gmail.com>
2023-10-22 17:37:40 -0600

    Changed order of parameters in `optika.metrology.SlopeErrorParameters`.

--- optika/metrology/_slope_error.py
+++ optika/metrology/_slope_error.py
@@ -18,21 +18,21 @@ class AbstractSlopeErrorParameters(
 
     @property
     @abc.abstractmethod
+    def step_size(self) -> na.ScalarLike:
+        """the horizontal distance to use when measuring the slope"""
+
+    @property
+    @abc.abstractmethod
     def kernel_size(self) -> na.ScalarLike:
         """
         size of the boxcar kernel that is convolved with the wavefront error
         before measuring the slope
         """
 
-    @property
-    @abc.abstractmethod
-    def step_size(self) -> na.ScalarLike:
-        """the horizontal distance to use when measuring the slope"""
-
 
 @dataclasses.dataclass(eq=False, repr=False)
 class SlopeErrorParameters(
     AbstractSlopeErrorParameters,
 ):
-    kernel_size: na.ScalarLike = 0 * u.mm
     step_size: na.ScalarLike = 0 * u.mm
+    kernel_size: na.ScalarLike = 0 * u.mm
--- optika/metrology/_tests/test_slope_error.py
+++ optika/metrology/_tests/test_slope_error.py
@@ -7,21 +7,21 @@ import optika
 
 
 class AbstractTestAbstractSlopeErrorParameters(abc.ABC):
-    def test_kernel_size(self, a: optika.metrology.AbstractSlopeErrorParameters):
-        assert np.issubdtype(na.get_dtype(a.kernel_size), float)
-        assert na.unit_normalized(a.kernel_size).is_equivalent(u.mm)
-
     def test_step_size(self, a: optika.metrology.AbstractSlopeErrorParameters):
         assert np.issubdtype(na.get_dtype(a.step_size), float)
         assert na.unit_normalized(a.step_size).is_equivalent(u.mm)
+
+    def test_kernel_size(self, a: optika.metrology.AbstractSlopeErrorParameters):
+        assert np.issubdtype(na.get_dtype(a.kernel_size), float)
+        assert na.unit_normalized(a.kernel_size).is_equivalent(u.mm)
 
 
 @pytest.mark.parametrize(
     argnames="a",
     argvalues=[
         optika.metrology.SlopeErrorParameters(
-            kernel_size=2 * u.mm,
             step_size=4 * u.mm,
+            kernel_size=2 * u.mm,
         )
     ],
 )
