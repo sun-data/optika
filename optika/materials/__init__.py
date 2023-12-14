a02ca5e6b59ef6e8ce2691c587caa887de3c47e7
Roy Smart <roytsmart@gmail.com>
2023-12-14 11:09:53 -0700

    Added a short description of the `optika.materials` subpackage.

--- optika/materials/__init__.py
+++ optika/materials/__init__.py
@@ -1,3 +1,7 @@
+"""
+A subpackage for simulating the interaction of light with transparent
+or reflective materials.
+"""
 from . import profiles
 from ._materials import *
 from ._materials_windt import *
