2ccc78dd9000ca09aa76a93c6c4c9bf6b413cca9
Roy Smart <roytsmart@gmail.com>
2023-12-14 19:22:22 -0700

    Added `optika.materials.snells_law()`

--- optika/surfaces.py
+++ optika/surfaces.py
@@ -123,176 +123,6 @@ class AbstractSurface(
         ----------
         rays
             a set of input rays that will interact with this surface
-
-        Notes
-        -----
-
-        Our goal is to derive a 3D version of Snell's law that can model the
-        diffraction from a periodic ruling pattern (diffraction grating).
-
-        To start, consider an incident wave of the form:
-
-        .. math::
-            :label: incident-wave
-
-            E_1(\mathbf{r}) = A_1 e^{i \mathbf{k}_1 \cdot \mathbf{r}},
-
-        where :math:`E_1` is the magnitude of the incident electric field,
-        :math:`A_1` is the amplitude of the incident wave, and :math:`\mathbf{k}_1` is
-        the incident wavevector.
-
-        Now define an interface at :math:`z = 0`, where the index of
-        refraction changes, and/or there is a periodic ruling pattern inscribed.
-        When the incident wave interacts with this interface, it will create an
-        output wave of the form:
-
-        .. math::
-            :label: transmitted-wave
-
-            E_2(\mathbf{r}) = A_2 e^{i \mathbf{k}_2 \cdot \mathbf{r}},
-
-        where :math:`E_2` is the magnitude of the output electric field,
-        :math:`A_2` is the amplitude of the output wave, and :math:`\mathbf{k}_2` is
-        the output wavevector.
-
-        Note in this case we care about only the reflected `or` the transmitted
-        wave, not both, since we're only concerned with sequential optics.
-
-        If the interface at :math:`z = 0` `doesn't` have a periodic ruling pattern,
-        :math:`E_1` and :math:`E_2` satisfy homogenous Dirichlet boundary conditions.
-
-        .. math::
-            :label: boundary-condition
-
-            A_1 \exp\left[i \mathbf{k}_1 \cdot (x \hat{\mathbf{x}} + y \hat{\mathbf{y}}) \right]
-            = A_2 \exp\left[i \mathbf{k}_2 \cdot (x \hat{\mathbf{x}} + y \hat{\mathbf{y}}) \right]
-
-        To include the ruling pattern, we model it as a phase shift of the wave at the interface,
-
-        .. math::
-            :label: phase-shift
-
-            \phi(x, y) = i \boldsymbol{\kappa} \cdot (x \hat{\mathbf{x}} + y \hat{\mathbf{y}})
-
-        where the vector
-
-        .. math::
-
-            \boldsymbol{\kappa} = -\frac{2 \pi m}{d} \hat{\boldsymbol{\kappa}},
-
-        :math:`m` is the diffraction order,
-        :math:`d` is the groove spacing,
-        and :math:`\hat{\boldsymbol{\kappa}}` is a unit vector normal to the
-        planes of the rulings.
-
-        With the inclusion of Equation :eq:`phase-shift`, Equation :eq:`boundary-condition` becomes:
-
-        .. math::
-            :label: boundary-condition-shifted
-
-             A_1 \exp\left[i (\mathbf{k}_1 + \boldsymbol{\kappa}) \cdot (x \hat{\mathbf{x}} + y \hat{\mathbf{y}}) \right]
-            = A_2 \exp\left[i \mathbf{k}_2 \cdot (x \hat{\mathbf{x}} + y \hat{\mathbf{y}}) \right]
-
-        For Equation :eq:`boundary-condition-shifted` to be true everywhere in
-        the :math:`x`-:math:`y` plane, the exponents must be equal:
-
-        .. math::
-            :label: boundary-condition-exponents
-
-            (\mathbf{k}_1 + \boldsymbol{\kappa}) \cdot (x \hat{\mathbf{x}} + y \hat{\mathbf{y}})
-            = \mathbf{k}_2 \cdot (x \hat{\mathbf{x}} + y \hat{\mathbf{y}}).
-
-        If we take :math:`\mathbf{k}_i = k_i \hat{\mathbf{k}}_i = n_i k_0 \hat{\mathbf{k}}_i` for :math:`i=(1,2)`,
-        where :math:`k_i` is the incident/output wavenumber,
-        :math:`n_i` is the incident/output index of refraction,
-        :math:`k_0` is the wavenumber in vacuum,
-        and :math:`\hat{\mathbf{k}}_i` is the incident/output propagation direction,
-        we get an expression in terms of the output direction, :math:`\hat{\mathbf{k}}_2`:
-
-        .. math::
-            :label: boundary-directions
-
-            n_1 (\hat{\mathbf{k}}_1 + \boldsymbol{\kappa} / k_1) \cdot (x \hat{\mathbf{x}} + y \hat{\mathbf{y}})
-            = n_2 \hat{\mathbf{k}}_2 \cdot (x \hat{\mathbf{x}} + y \hat{\mathbf{y}}).
-
-        Now, Equation :eq:`boundary-directions` can only be satisfied
-        if the components are separately equal since if :math:`y=0`
-
-        .. math::
-            :label: k_x
-
-            n_1 (\hat{\mathbf{k}}_1 + \boldsymbol{\kappa} / k_1) \cdot \hat{\mathbf{x}}
-            = n_2 \hat{\mathbf{k}}_2 \cdot \hat{\mathbf{x}},
-
-        and if :math:`x=0`
-
-        .. math::
-            :label: k_y
-
-            n_1 (\hat{\mathbf{k}}_1 + \boldsymbol{\kappa} / k_1) \cdot \hat{\mathbf{y}}
-            = n_2 \hat{\mathbf{k}}_2 \cdot \hat{\mathbf{y}}.
-
-        So, if we define an effective incident propagation direction
-
-        .. math::
-
-            \mathbf{k}_\text{e} = \hat{\mathbf{k}}_1 + \boldsymbol{\kappa} / k_1,
-
-        we can collect Equations :eq:`k_x` and :eq:`k_y` into a single vector
-        equation
-
-        .. math::
-            :label: snells-law
-
-            n_1 [ \mathbf{k}_\text{e} - ( \mathbf{k}_\text{e} \cdot \hat{\mathbf{n}} ) \hat{\mathbf{n}} ]
-            = n_2 [ \hat{\mathbf{k}}_2 - ( \hat{\mathbf{k}}_2 \cdot \hat{\mathbf{n}} ) \hat{\mathbf{n}} ],
-
-        where :math:`\hat{\mathbf{n}} = \hat{\mathbf{z}}` is the vector normal to
-        the interface.
-
-        Our goal now is to solve Equation :eq:`snells-law` for
-        the output propagation direction, :math:`\hat{\mathbf{k}}_2`,
-        and the only unknown is the component of the output propagation direction
-        parallel to the surface normal vector, :math:`(\hat{\mathbf{k}}_2 \cdot \hat{\mathbf{n}} )`.
-        We can write this in terms of a cross product,
-
-        .. math::
-            :label: k2_dot_n
-
-
-            \hat{\mathbf{k}}_2 \cdot \hat{\mathbf{n}} = \pm \sqrt{1 - |\hat{\mathbf{k}}_2 \times \hat{\mathbf{n}}|^2},
-
-        where the :math:`\pm` represents a transmission or reflection, and the
-        cross product :math:`\mathbf{k}_2 \times \hat{\mathbf{n}}` can be found by crossing
-        Equation :eq:`snells-law` with :math:`\hat{\mathbf{n}}`,
-
-        .. math::
-            :label: k2_cross_n
-
-            \hat{\mathbf{k}}_2 \times \hat{\mathbf{n}}
-            = \frac{n_1}{n_2} \mathbf{k}_\text{e} \times \hat{\mathbf{n}},
-
-        which leads to Equation :eq:`k2_dot_n` becoming:
-
-        .. math::
-            :label: k2_dot_n_expanded
-
-            \mathbf{k}_2 \cdot \hat{\mathbf{n}}
-            &= \pm \sqrt{1 - \left( \frac{n_1}{n_2} \right)^2 |\mathbf{k}_\text{e} \times \hat{\mathbf{n}}|^2} \\
-            &= \pm \frac{n_1}{n_2} \sqrt{\left( n_2 / n_1 \right)^2 + (\mathbf{k}_\text{e} \cdot \hat{\mathbf{n}})^2 - k_\text{e}^2 }
-
-        By plugging Equation :eq:`k2_dot_n_expanded` into Equation :eq:`snells-law`,
-        and solving for :math:`\hat{\mathbf{k}}_2`, we achieve our goal, an expression for the output ray
-        in terms of the input ray and other known quantities.
-
-        .. math::
-
-            \hat{\mathbf{k}}_2
-            = \frac{n_1}{n_2} \left[ \mathbf{k}_\text{e}
-            + \left(
-                \left( -\mathbf{k}_\text{e} \cdot \hat{\mathbf{n}} \right)
-                \pm \sqrt{\left( n_2 / n_1 \right)^2 + (\mathbf{k}_\text{e} \cdot \hat{\mathbf{n}})^2 - k_\text{e}^2 }
-            \right) \hat{\mathbf{n}} \right]
         """
         sag = self.sag
         material = self.material
@@ -320,19 +150,27 @@ class AbstractSurface(
         wavelength_2 = wavelength_1 / r
 
         if rulings is not None:
-            m = rulings.diffraction_order
-            d = rulings.spacing(position_1)
-            g = rulings.normal(position_1)
-            a = a - np.sign(a.z) * (m * wavelength_2 * g) / (n1 * d)
+            diffraction_order = rulings.diffraction_order
+            spacing_rulings = rulings.spacing(position_1)
+            normal_rulings = rulings.normal(position_1)
+        else:
+            diffraction_order = 0
+            spacing_rulings = None
+            normal_rulings = None
 
         normal = sag.normal(position_1)
 
-        c = -a @ normal
-
-        mirror = np.sign(a.z) * (2 * material.is_mirror - 1)
-
-        t = np.sqrt(np.square(1 / r) + np.square(c) - np.square(a.length))
-        b = r * (a + (c + mirror * t) * normal)
+        b = optika.materials.snells_law(
+            wavelength=wavelength_1,
+            direction=a,
+            index_refraction=n1,
+            index_refraction_new=n2,
+            is_mirror=material.is_mirror,
+            normal=normal,
+            diffraction_order=diffraction_order,
+            spacing_rulings=spacing_rulings,
+            normal_rulings=normal_rulings,
+        )
 
         intensity_2 = intensity_1 * material.transmissivity(rays_1, normal)
         attenuation_2 = material.attenuation(rays_1)
