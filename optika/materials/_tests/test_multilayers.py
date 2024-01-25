df40bdfc47dcc7ca937b760bb1a1cf740f6dad9a
Roy Smart <roytsmart@gmail.com>
2024-01-25 10:30:49 -0700

    Fixed bug in `optika.materials.multilayer_efficiency()` (#8)
    Fixed bug in `optika.materials.multilayer_efficiency()` where the boundary conditions were not being handled properly.

--- optika/materials/_tests/test_multilayers.py
+++ optika/materials/_tests/test_multilayers.py
@@ -1,5 +1,6 @@
-import numpy as np
 import pytest
+import pathlib
+import numpy as np
 import astropy.units as u
 import named_arrays as na
 import optika
@@ -89,6 +90,101 @@ def test_multilayer_efficiency(
     assert np.all(reflected + transmitted <= 1)
     assert np.all(np.imag(reflected) == 0)
     assert np.all(np.imag(transmitted) == 0)
+
+
+@pytest.mark.parametrize(
+    argnames=[
+        "file",
+        "material_layers",
+        "thickness_layers",
+        "axis_layers",
+        "direction_ambient",
+        "n_ambient",
+        "material_substrate",
+        "normal",
+        "profile_interface",
+    ],
+    argvalues=[
+        (
+            pathlib.Path(__file__).parent / "_data/Si.txt",
+            na.ScalarArray(np.array([]), axes="layer"),
+            na.ScalarArray([] * u.AA, axes="layer"),
+            "layer",
+            na.Cartesian3dVectorArray(0, 0, 1),
+            1,
+            "Si",
+            None,
+            None,
+        ),
+        (
+            pathlib.Path(__file__).parent / "_data/SiO2.txt",
+            na.ScalarArray(np.array(["SiO2"]), axes="layer"),
+            na.ScalarArray([50] * u.AA, axes="layer"),
+            "layer",
+            na.Cartesian3dVectorArray(0, 0, 1),
+            1,
+            "Si",
+            None,
+            None,
+        ),
+        (
+            pathlib.Path(__file__).parent / "_data/SiO2_100A.txt",
+            na.ScalarArray(np.array(["SiO2"]), axes="layer"),
+            na.ScalarArray([100] * u.AA, axes="layer"),
+            "layer",
+            na.Cartesian3dVectorArray(0, 0, 1),
+            1,
+            "Si",
+            None,
+            None,
+        ),
+    ],
+)
+def test_multilayer_transmissivity_vs_file(
+    file: pathlib.Path,
+    material_layers: na.AbstractScalarArray,
+    thickness_layers: na.AbstractScalarArray,
+    axis_layers: str,
+    direction_ambient: na.AbstractCartesian3dVectorArray,
+    n_ambient: complex | na.AbstractScalar,
+    material_substrate: str,
+    normal: None | na.AbstractCartesian3dVectorArray,
+    profile_interface: None | optika.materials.profiles.AbstractInterfaceProfile,
+):
+    wavelength_ambient, transmissivity_file = np.genfromtxt(
+        fname=file,
+        skip_header=15,
+        unpack=True,
+    )
+    wavelength_ambient = na.ScalarArray(wavelength_ambient, axes=axis_layers) << u.AA
+    transmissivity_file = na.ScalarArray(transmissivity_file, axes=axis_layers)
+
+    substrate = optika.chemicals.Chemical(material_substrate)
+    index_refraction_substrate = na.interp(
+        x=wavelength_ambient,
+        xp=substrate.index_refraction.inputs,
+        fp=substrate.index_refraction.outputs,
+    )
+    wavenumber_substrate = na.interp(
+        x=wavelength_ambient,
+        xp=substrate.wavenumber.inputs,
+        fp=substrate.wavenumber.outputs,
+    )
+    n_substrate = index_refraction_substrate + wavenumber_substrate * 1j
+
+    reflectivity, transmissivity = optika.materials.multilayer_efficiency(
+        material_layers=material_layers,
+        thickness_layers=thickness_layers,
+        axis_layers=axis_layers,
+        wavelength_ambient=wavelength_ambient,
+        direction_ambient=direction_ambient,
+        n_ambient=n_ambient,
+        n_substrate=n_substrate,
+        normal=normal,
+        profile_interface=profile_interface,
+    )
+
+    assert np.allclose(transmissivity, transmissivity_file, rtol=1e-4)
 
 
 class AbstractTestAbstractMultilayerMaterial(
