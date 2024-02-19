4d975845eebaba1e65ac06fad564bf9e24576e39
Roy Smart <roytsmart@gmail.com>
2024-02-19 15:22:04 -0700

    Added `optika.materials.matrix_refractive()` function.

--- optika/materials/_tests/test_multilayers.py
+++ optika/materials/_tests/test_multilayers.py
@@ -1,3 +1,4 @@
+from typing import Literal
 import pytest
 import pathlib
 import numpy as np
@@ -7,6 +8,41 @@ import optika
 from . import test_materials
 
 _wavelength = na.linspace(100, 200, axis="wavelength", num=4) * u.AA
+
+
+@pytest.mark.parametrize("wavelength", [_wavelength])
+@pytest.mark.parametrize("direction_left", [na.Cartesian3dVectorArray(0, 0, 1)])
+@pytest.mark.parametrize("direction_right", [na.Cartesian3dVectorArray(0, 0, 1)])
+@pytest.mark.parametrize("polarization", ["s", "p"])
+@pytest.mark.parametrize("n_left", [1])
+@pytest.mark.parametrize("n_right", [optika.chemicals.Chemical("Si").n(_wavelength)])
+@pytest.mark.parametrize("normal", [na.Cartesian3dVectorArray(0, 0, -1)])
+@pytest.mark.parametrize(
+    argnames="interface",
+    argvalues=[optika.materials.profiles.ErfInterfaceProfile(5 * u.nm)],
+)
+def test_matrix_refractive(
+    wavelength: u.Quantity | na.AbstractScalar,
+    direction_left: na.AbstractCartesian3dVectorArray,
+    direction_right: na.AbstractCartesian3dVectorArray,
+    polarization: Literal["s", "p"],
+    n_left: float | na.AbstractScalar,
+    n_right: float | na.AbstractScalar,
+    normal: na.AbstractCartesian3dVectorArray,
+    interface: None | optika.materials.profiles.AbstractInterfaceProfile,
+):
+    result = optika.materials.matrix_refractive(
+        wavelength=wavelength,
+        direction_left=direction_left,
+        direction_right=direction_right,
+        polarization=polarization,
+        n_left=n_left,
+        n_right=n_right,
+        normal=normal,
+        interface=interface,
+    )
+    assert isinstance(result, na.AbstractCartesian2dMatrixArray)
+    assert np.all(result.determinant != 0)
 
 
 @pytest.mark.parametrize(
