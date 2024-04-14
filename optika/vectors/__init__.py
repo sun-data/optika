349bbcc0c5a1e4b96f79d7532c44c4b74f9d6700
Roy Smart <roytsmart@gmail.com>
2024-04-13 21:23:40 -0600

    Fixing ruff errors.

--- optika/vectors/__init__.py
+++ optika/vectors/__init__.py
@@ -1,5 +1,33 @@
-from ._vectors_polarization import *
-from ._vectors_field import *
-from ._vectors_pupil import *
-from ._vectors_scene import *
-from ._vectors_object import *
+from ._vectors_polarization import (
+    AbstractPolarizationVectorArray,
+    PolarizationVectorArray,
+)
+from ._vectors_field import (
+    AbstractFieldVectorArray,
+    FieldVectorArray,
+)
+from ._vectors_pupil import (
+    AbstractPupilVectorArray,
+    PupilVectorArray,
+)
+from ._vectors_scene import (
+    AbstractSceneVectorArray,
+    SceneVectorArray,
+)
+from ._vectors_object import (
+    AbstractObjectVectorArray,
+    ObjectVectorArray,
+)
+
+__all__ = [
+    "AbstractPolarizationVectorArray",
+    "PolarizationVectorArray",
+    "AbstractFieldVectorArray",
+    "FieldVectorArray",
+    "AbstractPupilVectorArray",
+    "PupilVectorArray",
+    "AbstractSceneVectorArray",
+    "SceneVectorArray",
+    "AbstractObjectVectorArray",
+    "ObjectVectorArray",
+]
