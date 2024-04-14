0daa4fb6564400c454b17aaf907bb777348975ee
Roy Smart <roytsmart@gmail.com>
2024-04-13 20:36:50 -0600

    Fixing ruff errors.

--- optika/materials/__init__.py
+++ optika/materials/__init__.py
@@ -3,11 +3,56 @@ A subpackage for simulating the interaction of light with transparent
 or reflective materials.
 """
 
-from ._snells_law import *
+from ._snells_law import snells_law
 from . import profiles
 from . import matrices
-from ._layers import *
-from ._materials import *
-from ._multilayers import *
+from ._layers import (
+    AbstractLayer,
+    Layer,
+    AbstractLayerSequence,
+    LayerSequence,
+    PeriodicLayerSequence,
+)
+from ._materials import (
+    AbstractMaterial,
+    Vacuum,
+    AbstractMirror,
+    Mirror,
+    MeasuredMirror,
+)
+from ._multilayers import (
+    multilayer_efficiency,
+    AbstractMultilayerFilm,
+    MultilayerFilm,
+    AbstractMultilayerMirror,
+    MultilayerMirror,
+)
 from . import meshes
-from ._thin_films import *
+from ._thin_films import (
+    AbstractThinFilmFilter,
+    ThinFilmFilter,
+)
+
+__all__ = [
+    "snells_law",
+    "profiles",
+    "matrices",
+    "AbstractLayer",
+    "Layer",
+    "AbstractLayerSequence",
+    "LayerSequence",
+    "PeriodicLayerSequence",
+    "AbstractMaterial",
+    "Vacuum",
+    "AbstractMirror",
+    "Mirror",
+    "MeasuredMirror",
+    "multilayer_efficiency",
+    "AbstractMultilayerFilm",
+    "MultilayerFilm",
+    "AbstractMultilayerMirror",
+    "MultilayerMirror",
+    "meshes",
+    "AbstractThinFilmFilter",
+    "ThinFilmFilter",
+]
