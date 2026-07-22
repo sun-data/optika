edda0fe5639d207a8f2e4d1479a8228fd8e5b90f
Roy Smart <roytsmart@gmail.com>
2026-07-21 20:26:26 -0600

    fixes

--- optika/systems/__init__.py
+++ optika/systems/__init__.py
@@ -3,13 +3,13 @@ Optical systems consisting of multiple optical surfaces.
 """
 
 from ._systems import AbstractSystem
-from ._sequential import AbstractSequentialSystem, SequentialSystem
 from ._linear import AbstractLinearSystem, LinearSystem
+from ._sequential import AbstractSequentialSystem, SequentialSystem
 
 __all__ = [
     "AbstractSystem",
-    "AbstractSequentialSystem",
-    "SequentialSystem",
     "AbstractLinearSystem",
     "LinearSystem",
+    "AbstractSequentialSystem",
+    "SequentialSystem",
 ]
